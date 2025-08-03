7cae033b8299f9ddce4bd4ccd826d554e6646cbe
Sergey-8057 <erste_sergey@proton.me>
2025-08-03 03:29:06 +0300

    update-7 file app/notes/[id]/page.tsx

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,19 +2,17 @@ import { fetchNotes } from '../../lib/api';
 import NotesClient from './Notes.client';
 
 interface NotesPageProps {
-  searchParams: {
+  searchParams: Promise<{
     search?: string;
     page?: string;
-  };
+  }>;
 }
 
 export default async function NotesPage({ searchParams }: NotesPageProps) {
-  const resolvedParams = await Promise.resolve(searchParams);
-
+  const resolvedParams = await searchParams;
   const search = resolvedParams.search || '';
   const page = Number(resolvedParams.page) || 1;
   const perPage = 12;
-
   const initialData = await fetchNotes(search, page, perPage);
 
   return <NotesClient initialData={initialData} initialSearchParams={{ search, page }} />;
