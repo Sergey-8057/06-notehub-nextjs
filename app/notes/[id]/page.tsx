c7bdb84cbe741b2eeaa972fc0d2cd787de2f947b
Sergey-8057 <erste_sergey@proton.me>
2025-08-03 03:43:22 +0300

    update-8 file app/notes/[id]/page.tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,15 +1,15 @@
 import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
-import { fetchNoteById } from '../../../lib/api';
+import { fetchNoteById } from '@/lib/api';
 import NoteDetailsClient from './NoteDetails.client';
 
-type Props = {
+interface NoteDetailsPageProps {
   params: Promise<{ id: string }>;
-};
+}
 
-const NoteDetailsPage = async ({ params }: Props) => {
+const NoteDetailsPage = async ({ params }: NoteDetailsPageProps) => {
   const { id } = await params;
-  const queryClient = new QueryClient();
 
+  const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: ['note', id],
     queryFn: () => fetchNoteById(id),
